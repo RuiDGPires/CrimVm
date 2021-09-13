d66158a00f2512e22c6c3819a6fc7f18d63f00b7
RuiDGPires <pires.rui@outlook.pt>
2021-09-13 11:54:45 +0100

    Define some error handling macros

--- src/util.h
+++ src/util.h
@@ -3,5 +3,38 @@
 
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef DEBUG
+#ifndef __FUNCTION_NAME__ 
+	#ifdef WIN32   //WINDOWS
+		#define __FUNCTION_NAME__   __FUNCTION__  
+	#else          //*NIX
+		#define __FUNCTION_NAME__   __func__ 
+	#endif
+#endif
+
+#define EXTRA() fprintf(stderr, "%s: ", __FUNCTION_NAME__)
+#else
+#define EXTRA() ;
+#endif
+
+#define THROW_ERROR(...) \
+    do{\
+			fflush(stdout);\
+			EXTRA(); \
+			fprintf(stderr, __VA_ARGS__); \
+			fprintf(stderr, "\n");\
+			if (errno)\
+				fprintf(stderr, "\t%s\n", strerror(errno));\
+			exit(-1);\
+    }while(0)
+
+#define ASSERT(expression, ...) \
+	do{\
+	if (expression);\
+	else THROW_ERROR(__VA_ARGS__);\
+	}while(0)
 
 #endif
