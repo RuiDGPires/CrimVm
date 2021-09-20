c27e6773c3586803e795f0913c1e01e70b3f3ae2
RuiDGPires <pires.rui@outlook.pt>
2021-09-20 16:16:56 +0100

    Add another register

--- src/vm.h
+++ src/vm.h
@@ -42,7 +42,7 @@ typedef struct {
 } Operation;
 
 // R0 is ALWAYS equal to 0. Other registers include 3 general purpose registers, a stack pointer and a return register.
-enum reg_type{R0 = 0, R1, R2, R3, SP, RE, R_COUNT};
+enum reg_type{R0 = 0, R1, R2, R3, R4, SP, RE, R_COUNT};
 typedef struct vm_s {
 	Loader loader;
 	// MemManager memmanager;
