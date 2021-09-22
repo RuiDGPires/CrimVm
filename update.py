95a5ab6fc439d10d04b32b2d68810e26520245cd
RuiDGPires <pires.rui@outlook.pt>
2021-09-22 11:48:57 +0100

    Get update script to master branch too

---
+++ update.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from glob import glob
+from subprocess import run
+
+files = glob("src/*")
+
+
+run(["git", "checkout", "core"])
+run(["git", "checkout", "master", "--"] + files)
+run(["git", "commit", "-m", "\"Update files from master\""])
